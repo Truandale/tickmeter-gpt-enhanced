0319d4428e53a75b1789633aa9424384b0c12eaf
Andrey <livevil@live.ru>
2025-02-16 12:55:59 +0300

    Этап 2.
    Добавлен файл NetworkRecords.cs с классами для процессов
    
    В проекте `tickMeter.csproj` был добавлен файл `NetworkRecords.cs`, который содержит классы для записи информации о TCP и UDP процессах.
    
    Добавлены два класса: `TcpProcessRecord` и `UdpProcessRecord`.
    Класс `TcpProcessRecord` включает свойства для хранения информации о локальном и удаленном адресах, портах, идентификаторе процесса и его имени, а также состоянии соединения.
    Класс `UdpProcessRecord` содержит свойства для хранения информации о локальном адресе, порте, идентификаторе процесса и его имени.
    Оба класса имеют конструкторы, которые инициализируют их свойства.

---
+++ tickMeter/Classes/NetworkRecords.cs
@@ -0,0 +1,40 @@
+﻿namespace TickMeter
+{
+    public class TcpProcessRecord
+    {
+        public string LocalAddress { get; set; }
+        public int LocalPort { get; set; }
+        public string RemoteAddress { get; set; }
+        public int RemotePort { get; set; }
+        public int ProcessId { get; set; }
+        public string ProcessName { get; set; }
+        public string State { get; set; }
+
+        public TcpProcessRecord(string localAddress, int localPort, string remoteAddress, int remotePort, int processId, string processName, string state)
+        {
+            LocalAddress = localAddress;
+            LocalPort = localPort;
+            RemoteAddress = remoteAddress;
+            RemotePort = remotePort;
+            ProcessId = processId;
+            ProcessName = processName;
+            State = state;
+        }
+    }
+
+    public class UdpProcessRecord
+    {
+        public string LocalAddress { get; set; }
+        public int LocalPort { get; set; }
+        public int ProcessId { get; set; }
+        public string ProcessName { get; set; }
+
+        public UdpProcessRecord(string localAddress, int localPort, int processId, string processName)
+        {
+            LocalAddress = localAddress;
+            LocalPort = localPort;
+            ProcessId = processId;
+            ProcessName = processName;
+        }
+    }
+}
