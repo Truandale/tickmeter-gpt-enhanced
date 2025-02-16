289a0659fe9f35cafd13087a90f41ebfbd7f9a2c
Andrey <livevil@live.ru>
2025-02-16 13:43:26 +0300

    Этап 2 Добавлены классы для обработки TCP и UDP соединений
    В проекте `tickMeter.csproj` добавлены файлы `TcpProcessRecord.cs` и `UdpProcessRecord.cs`.
    Класс `TcpProcessRecord` содержит свойства для хранения информации о TCP-соединениях, включая локальные и удаленные адреса, порты, идентификатор и имя процесса.
    Класс `UdpProcessRecord` аналогично хранит данные для UDP-соединений. Оба класса имеют конструкторы для инициализации своих свойств.

---
+++ tickMeter/Classes/TcpProcessRecord.cs
@@ -0,0 +1,21 @@
+﻿public class TcpProcessRecord
+{
+    public string LocalAddress { get; set; }
+    public int LocalPort { get; set; }
+    public string RemoteAddress { get; set; }
+    public int RemotePort { get; set; }
+    public int ProcessId { get; set; }
+    public string ProcessName { get; set; }
+    public string State { get; set; }
+
+    public TcpProcessRecord(string localAddress, int localPort, string remoteAddress, int remotePort, int processId, string processName, string state)
+    {
+        LocalAddress = localAddress;
+        LocalPort = localPort;
+        RemoteAddress = remoteAddress;
+        RemotePort = remotePort;
+        ProcessId = processId;
+        ProcessName = processName;
+        State = state;
+    }
+}
---
+++ tickMeter/Classes/UdpProcessRecord.cs
@@ -0,0 +1,15 @@
+﻿public class UdpProcessRecord
+{
+    public string LocalAddress { get; set; }
+    public int LocalPort { get; set; }
+    public int ProcessId { get; set; }
+    public string ProcessName { get; set; }
+
+    public UdpProcessRecord(string localAddress, int localPort, int processId, string processName)
+    {
+        LocalAddress = localAddress;
+        LocalPort = localPort;
+        ProcessId = processId;
+        ProcessName = processName;
+    }
+}
